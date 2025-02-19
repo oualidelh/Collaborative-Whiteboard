c4cd1c6bd58126bf3c5b1b014586e78b23d129da
oualidelh <elhouarioualid@gmail.com>
2025-02-19 09:27:10 +0100

    setup and run teh server

---
+++ server/src/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {});
+
+server.listen(5000, () => {
+  console.log("server running in port 5000");
+});
